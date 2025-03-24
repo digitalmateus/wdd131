53b8e4acaeee2c995a91134853285c6f13f40eb6
digitalmateus <mnunes@byupathway.edu>
2025-03-24 17:15:40 -0300

    lazy load and other

---
+++ week04/scripts/lazy.js
@@ -0,0 +1,8 @@
+
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified; 
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
+});
