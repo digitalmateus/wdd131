217a6c3e46fc480640919d25aea4a7f72a264e93
digitalmateus <mnunes@byupathway.edu>
2025-03-24 20:56:11 -0300

    corrections

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,13 @@
 const currentYear = new Date().getFullYear();
 const lastModified = document.lastModified; 
 
+const mainnav = document.querySelector(`.navigation`)
+const hambutton = document.querySelector(`#menu`);
+
+hambutton.addEventListener(`click`,() => {
+    mainnav.classList.toggle(`show`);
+    hambutton.classList.toggle(`show`);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("currentYear").textContent = new Date().getFullYear();
