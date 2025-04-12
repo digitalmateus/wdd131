cabc076023b36d8465a67d876e2269bfa1e3a26b
digitalmateus <mnunes@byupathway.edu>
2025-04-12 19:56:03 -0300

    changes

--- project/scripts/course.js
+++ project/scripts/course.js
@@ -1,6 +1,19 @@
 const currentYear = new Date().getFullYear();
 const lastModified = document.lastModified; 
 
+// form validation
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const plan = document.getElementById("plan").value;
+
+    if (name === "" || email === "" || plan === "") {
+        alert("Please fill out all fields.");
+        return false;
+    }
+    return true;
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("currentYear").textContent = new Date().getFullYear();
